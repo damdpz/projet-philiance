503e10bca8eec9de4e8dc5762b373e1269c094aa
Damien Deprez <1damiendeprez@gmail.com>
2023-07-10 15:46:05 +0200

    modif 10/07

--- inc/footer.inc.php
+++ inc/footer.inc.php
@@ -1,4 +1,29 @@
-<footer></footer>
+<footer>
+    <div>
+    <img src="assets/img/logo/LOGO_DAIKLES.jpg" alt="logo">
+    </div>
+
+    <form action="">
+        <label for=""></label>
+        <input type="text">
+        <input type="submit">
+    </form>
+
+    <div class="nav-footer">
+        <ul>
+        <li><a href="homme.php">Homme</a></li>
+            <li><a href="homme.php">Femme</a></li>
+            <li><a href="homme.php">Sneakers</a></li>
+            <li><a href="homme.php">Accessoires</a></li>
+            <li><a href="compte.php">Mon compte</a></li>
+        </ul>
+    </div>
+
+
+
+
+
+</footer>
 <script src="index.js"></script>
 
 <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script> -->
