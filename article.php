a0ab687b33ae9b2693aacb681d5f6ae83bba069f
ikrame <ikr.projet@gmail.com>
2023-07-12 17:03:06 +0200

    ajout article

--- article.php
+++ article.php
@@ -14,11 +14,63 @@ require_once("inc/nav.inc.php");
 
 
 <main>
+    <section>
+        <div class="product">
+            <image src="../PROJET-PHILIANCE/assets/img/nouveautes/pull.jpg" alt="pull">
+        </div>
+        
+            <div class="name-product">
+                <h2>SWEETSHIIRTS - CREW NECK</h2>
+            </div><br>  
+                                
+                <div class="order">
+                    <a href="#" class="price-product">199.99€</a>
+                        <h2>ADD TO MY BAG</h2>
+                </div><br>
 
+                    <div class="description product">
+                        <h2>DESCRIPTION</h2>
+                            <p> The product C.P. Company - SWEATSHIRTS - CREW NECK in the color CEDAR WOOD is available at BSTN.
+                                Use our Wishlist feature to get notified when the product or your desired size is available.
+                                Article Number:
+	                            15CMSS022A005086W-476
+                                Gender:
+	                            Men
+                                Color:
+	                            CEDAR WOOD
+                                Material:
+	                            Full: 100% Cotton</p><br>
+    </section>
+<section>
+    <div class="title-product">
+        <h2>PICK UP FOR YOU</h2>
+    </div><br>
 
+    <div class="product">
+        <div class="product-for-you">
+            <image src="../PROJET-PHILIANCE/assets/img/nouveautes/pantalon.webp" alt="pantalon">
+                <div class ="type-product-for-you">
+                    <a href="#" class="btn-product">Carhartt WIP Nolan Pant</a>
+        </div>
+                    <a href="#" class="btn-price-product">89.99€</a>
+        </div>
 
+    <div class="product-for-you">
+        <image src="../PROJET-PHILIANCE/assets/img/nouveautes/veste.webap" alt="veste">
+            <div class="type-product-for-you">
+                <a href="#" class="btn-product">X WALES BONNER KNIT TRACK</a>
+    </div>
+                <a href="#" class="btn-price-product">279.99€</a>
+    </div>
 
-
+    <div class="product-for-you">
+        <image src="../PROJET-PHILIANCE/assets/img/nouveautes/sneakers.jpg" alt="sneakers">
+            <div class="type-product-for-you">
+                <a href="#" class="btn-product">Nike WMNS DUNK LOW LX</a>
+    </div>
+                <a href="#" class="btn-price-product">119.99€</a>
+    </div><br>
+</section>
 </main>
 
 <?php
