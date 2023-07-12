5a9b34ced3a697d2afba55343c9691930e313242
ikrame <ikr.projet@gmail.com>
2023-07-12 17:12:42 +0200

    ajout article

--- article.php
+++ article.php
@@ -3,7 +3,7 @@ require_once("inc/init.inc.php");
 require_once("inc/function.inc.php");
 
 
-// // CODE PROPRE A LA PAGE PHP EN COURSS
+// // CODE PROPRE A LA PAGE PHP EN COURS
 // echo '<link href="assets/css/index.css" rel="stylesheet" type="text/css"';
 
 require_once("inc/head.inc.php");
