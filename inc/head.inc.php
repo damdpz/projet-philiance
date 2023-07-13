42abf7209bda5fbfacb1cf6ffddd26215ee32d9c
Mehdi AISSANI <contact@mehdiaissani.com>
2023-07-13 13:59:54 +0200

    chore(header): fix css imports

--- inc/head.inc.php
+++ inc/head.inc.php
@@ -9,12 +9,12 @@
     <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous"> -->
     <meta description="Site street wear">
     <!-- Intégration du CSS au PHP -->
-    <link rel="stylesheet" href="../PROJET_PHILIANCE/assets/css/index.css">
-    <link rel="stylesheet" href="../PROJET_PHILIANCE/assets/css/article.css">
-    <link rel="stylesheet" href="../PROJET_PHILIANCE/assets/css/category.css">
-    <link rel="stylesheet" href="../PROJET_PHILIANCE/assets/css/compte.css">
-    <link rel="stylesheet" href="../PROJET_PHILIANCE/assets/css/panier.css">
-    <link rel="shortcut icon" href="../assets/img/logo/LOGO_DAIKLES.jpg">
-    <script defer src="../assets/js/index.js"></script>
+    <link rel="stylesheet" href="assets/css/index.css">
+    <link rel="stylesheet" href="assets/css/article.css">
+    <link rel="stylesheet" href="assets/css/category.css">
+    <link rel="stylesheet" href="assets/css/compte.css">
+    <link rel="stylesheet" href="assets/css/panier.css">
+    <link rel="shortcut icon" href="assets/img/logo/LOGO_DAIKLES.jpg">
+    <script defer src="assets/js/index.js"></script>
     <title>DAIKLES</title>
 </head>
