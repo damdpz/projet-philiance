94537c9ec9970ed6f043c7c502472756fee03721
Damien Deprez <1damiendeprez@gmail.com>
2023-07-18 15:02:19 +0200

    modif 18/07

--- article.php
+++ article.php
@@ -17,59 +17,63 @@ require_once("inc/category.inc.php");
 <main>
     <section>
         <div class="article-container">
-            <image src="../PROJET_PHILIANCE/assets/img/nouveautes/pull.jpg" alt="pull">
+            <div class="image-article">
+                <image src="../PROJET_PHILIANCE/assets/img/nouveautes/pull.jpg" alt="pull">
+            </div>
 
-                <div class="article-commander">
-                    <div class="title-article">
-                        <h3>SWEETSHIRTS - CREW NECK</h3>
-                    </div>
-                    <div class="btn-article">
-                        <a href="panier.php"><input type="submit" value="commander" class="form-control" aria-label="EURO amount (with dot and two decimal places)"></a>
-                        <span class="input-group-text">€</span>
-                        <span class="input-group-text">199.99</span>
-                    </div>
-
-
-
-                    <div class="description-product">
-                        <h3>DESCRIPTION</h3>
-                        <p> Le pull SWEATSHIRTS - CREW NECK de couleur rose est disponible sur DAIKLES.
-                            Ce pull oversize est 100% Cotton</p>
-                    </div>
-
+            <div class="description-article">
+                <div class="titre-article">
+                    <h3>SWEETSHIRTS - CREW NECK</h3>
                 </div>
+                <p> Le pull SWEATSHIRTS - CREW NECK de couleur bois de cèdre est disponible sur DAIKLES.
+                    Ce pull oversize est 100% Cotton</p>
 
+                <p>Numéro d'article: 15CMSS022AZE5086W-456
+                    <br>
+                    Sexe : Hommes
+                    <br>
+                    Couleur : Bois de cèdre
+                    <br>
+                    Matériel : 100% coton
+                </p>
+                <div class="btn-article">
+                    <span>199.99 €</span>
+                    <a href="panier.php"><input type="submit" value="Ajouter au panier"></a>
+                </div>
+            </div>
+        </div>
 
     </section>
 
     <section>
-        <div class="title-product">
+        <div class="title-selection">
             <h2>NOS SÉLÉCTIONS</h2>
         </div><br>
 
-        <div class="product">
-            <div class="product-for-you">
+        <div class="selection">
+            <div class="selection-article">
                 <image src="../PROJET_PHILIANCE/assets/img/nouveautes/pantalon.webp" alt="pantalon">
-                    <div class="type-product-for-you">
-                        <a href="#" class="btn-product">Carhartt WIP Nolan Pant</a>
+                    <div class="titre-selection">
+                        <a href="#" class="btn-selection">CARHARTT WIP NOLAN PANT
+                        </a>
                     </div>
-                    <a href="#" class="btn-price-product">89.99€</a>
+                    <a href="#" class="btn-price-selection">89.99€</a>
             </div>
 
-            <div class="product-for-you">
+            <div class="selection-article">
                 <image src="../PROJET_PHILIANCE/assets/img/nouveautes/veste.webp" alt="veste">
-                    <div class="type-product-for-you">
-                        <a href="#" class="btn-product">X WALES BONNER KNIT TRACK</a>
+                    <div class="titre-selection">
+                        <a href="#" class="btn-selection">X WALES BONNER KNIT TRACK</a>
                     </div>
-                    <a href="#" class="btn-price-product">279.99€</a>
+                    <a href="#" class="btn-price-selection">279.99€</a>
             </div>
 
-            <div class="product-for-you">
+            <div class="selection-article">
                 <image src="../PROJET_PHILIANCE/assets/img/nouveautes/sneakers.jpg" alt="sneakers">
-                    <div class="type-product-for-you">
-                        <a href="#" class="btn-product">Nike WMNS DUNK LOW LX</a>
+                    <div class="titre-selection">
+                        <a href="#" class="btn-selection">Nike WMNS DUNK LOW LX</a>
                     </div>
-                    <a href="#" class="btn-price-product">119.99€</a>
+                    <a href="#" class="btn-price-selection">119.99€</a>
             </div>
     </section>
 </main>
