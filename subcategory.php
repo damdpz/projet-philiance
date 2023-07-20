1ca10f1b1cb3846ff0232537651316a79d4b1f9a
Damien Deprez <1damiendeprez@gmail.com>
2023-07-20 18:35:30 +0200

    add subcategory.php

---
+++ subcategory.php
@@ -0,0 +1,92 @@
+<?php
+require_once("inc/init.inc.php");
+require_once("inc/function.inc.php");
+
+
+// // CODE PROPRE A LA PAGE PHP EN COURS
+// echo '<link href="assets/css/index.css" rel="stylesheet" type="text/css"';
+
+require_once("inc/head.inc.php");
+require_once("inc/nav.inc.php");
+require_once("inc/category.inc.php");
+
+?>
+
+<main>
+    <div class="title-subcat">
+    <h2>Accessoires</h2>
+    </div>
+    <div class="container-subcat">
+        <div class="subcat">
+            <a href=""><img src="assets/img/nouveautes/sneakers.jpg" alt="photo01"/></a>
+            <div class="content-subcat">
+                <a href="#" class="btn-subcat">
+                    <h3>WMNS DUNK LOW LX</h3>
+                </a>
+                <p>Nike</p>
+                <p>109.99€</p>
+            </div>
+        </div>
+
+        <div class="subcat">
+            <a href=""><img src="assets/img/nouveautes/sneakers.jpg" alt="photo01"/></a>
+            <div class="content-subcat">
+                <a href="#" class="btn-subcat">
+                    <h3>WMNS DUNK LOW LX</h3>
+                </a>
+                <p>Nike</p>
+                <p>109.99€</p>
+            </div>
+        </div>
+
+        <div class="subcat">
+            <a href=""><img src="assets/img/nouveautes/sneakers.jpg" alt="photo01"/></a>
+            <div class="content-subcat">
+                <a href="#" class="btn-subcat">
+                    <h3>WMNS DUNK LOW LX</h3>
+                </a>
+                <p>Nike</p>
+                <p>109.99€</p>
+            </div>
+        </div>
+
+        <div class="subcat">
+            <a href=""><img src="assets/img/nouveautes/sneakers.jpg" alt="photo01"/></a>
+            <div class="content-subcat">
+                <a href="#" class="btn-subcat">
+                    <h3>WMNS DUNK LOW LX</h3>
+                </a>
+                <p>Nike</p>
+                <p>109.99€</p>
+            </div>
+        </div>
+
+        <div class="subcat">
+            <a href=""><img src="assets/img/nouveautes/sneakers.jpg" alt="photo01"/></a>
+            <div class="content-subcat">
+                <a href="#" class="btn-subcat">
+                    <h3>WMNS DUNK LOW LX</h3>
+                </a>
+                <p>Nike</p>
+                <p>109.99€</p>
+            </div>
+        </div>
+
+        <div class="subcat">
+            <a href=""><img src="assets/img/nouveautes/sneakers.jpg" alt="photo01"/></a>
+            <div class="content-subcat">
+                <a href="#" class="btn-subcat">
+                    <h3>WMNS DUNK LOW LX</h3>
+                </a>
+                <p>Nike</p>
+                <p>109.99€</p>
+            </div>
+        </div>
+
+    </div>
+
+
+</main>
+
+<?php
+require_once("inc/footer.inc.php");
