80627612dd90e61d7d70c3aa062d52348ef0c4d8
Damien Deprez <1damiendeprez@gmail.com>
2023-07-23 12:48:08 +0200

    update compte.php & inscription.php

--- compte.php
+++ compte.php
@@ -15,35 +15,35 @@ require_once("inc/category.inc.php");
 
 <main>
     <div class="form1">
-        <img src="assets/img/image-compte/coucher-de-soleil.jpg" alt="background">
+
         <!--FORMULAIRE-->
- <form class="connexion">
-    <h1>Mon compte</h1>
+        <form class="connexion">
+            <h1>Mon compte</h1>
 
-    <div class="champ">
+            <div class="champ">
 
-        <input type="email" placeholder="Email"> <br>
-        <label for="email"></label>
+                <input type="email" placeholder="Email"> <br>
+                <label for="email"></label>
 
 
-        <input type="password" placeholder="Mot de passe"> <br>
-        <label for="password"></label>
-        
-        <a href="#" class="pw">Mot de passe oublié</a><br>
+                <input type="password" placeholder="Mot de passe"> <br>
+                <label for="password"></label>
 
-        <input type="button" value="Connexion" style="
-         margin-top: 20px;"> 
-    </div>
+                <a href="#" class="pw">Mot de passe oublié</a><br>
+                <a href="connexion.php">
+                <input class="btn-connexion" type="button" value="Connexion" style="
+         margin-top: 20px;"></a>
+            </div>
+        </form>
 
-   
-        <div class="champ1">
-           <h3>Pas de compte ?</h3>
-        
-           <a href="inscription.php" class="compte"><input type="button" value="Créer un compte"></a>
+        <div class="new-client">
+        <h4>Nouveau chez Daikles ?</h4>
         </div>
-    </form>
- </form>
-       
+            <div class="champ1">
+                <a href="inscription.php">
+                <input class="btn-create-compte"  type="button" value="Créer un compte"></a>
+            </div>
+
     </div>
 
 
