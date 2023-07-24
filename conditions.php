080e70e81e90764d759afde6ee6516b6b9cdaa10
Saida Boukeddah <sboukeddah@gmail.com>
2023-07-24 11:34:28 +0200

    git pull

---
+++ conditions.php
@@ -0,0 +1,47 @@
+<?php
+require_once("inc/init.inc.php");
+require_once("inc/function.inc.php");
+
+
+// // CODE PROPRE A LA PAGE PHP EN COURS
+echo '<link href="assets/css/compte.css" rel="stylesheet" type="text/css">';
+
+require_once("inc/head.inc.php");
+require_once("inc/nav.inc.php");
+require_once("inc/category.inc.php");
+
+?>
+
+<main>
+
+    <section>
+        <h2>Acceptation des conditions</h2>
+        <p>En accédant et en utilisant ce site web, vous acceptez d'être lié par ces conditions d'utilisation. Si vous n'acceptez pas ces conditions, veuillez ne pas utiliser ce site.</p>
+    </section>
+
+    <section>
+        <h2>Contenu du site</h2>
+        <p>Toutes les informations, textes, graphiques, images et autres éléments présents sur ce site sont fournis à titre informatif uniquement. L'utilisation du contenu du site est à vos propres risques.</p>
+        <p>Nous nous efforçons de fournir des informations précises et à jour, mais nous ne garantissons pas l'exactitude, l'exhaustivité ou la fiabilité du contenu du site.</p>
+    </section>
+
+    <section>
+        <h2>Utilisation du site</h2>
+        <p>Vous vous engagez à utiliser ce site conformément aux lois en vigueur et aux présentes conditions d'utilisation.</p>
+        <p>Il est interdit d'utiliser ce site pour des activités illégales, diffamatoires, offensantes, ou qui pourraient porter atteinte aux droits de tiers.</p>
+    </section>
+
+    <section>
+        <h2>Liens vers des sites tiers</h2>
+        <p>Ce site peut contenir des liens vers des sites web tiers. Nous n'assumons aucune responsabilité quant au contenu ou à la politique de confidentialité de ces sites tiers.</p>
+    </section>
+
+    <section>
+        <h2>Modification des conditions</h2>
+        <p>Nous nous réservons le droit de modifier ces conditions d'utilisation à tout moment. Les modifications prendront effet dès leur publication sur le site. Nous vous encourageons à consulter régulièrement cette page pour rester informé des éventuelles modifications.</p>
+    </section>
+
+</main>
+
+<?php
+require_once("inc/footer.inc.php");
---
+++ mentions-légales.php
@@ -0,0 +1,55 @@
+<?php
+require_once("inc/init.inc.php");
+require_once("inc/function.inc.php");
+
+
+// // CODE PROPRE A LA PAGE PHP EN COURS
+echo '<link href="assets/css/compte.css" rel="stylesheet" type="text/css">';
+
+require_once("inc/head.inc.php");
+require_once("inc/nav.inc.php");
+require_once("inc/category.inc.php");
+
+?>
+
+<main>
+
+    <section>
+        <h2>Informations légales</h2>
+        <p>Nom de l'entreprise : [Nom de l'entreprise]</p>
+        <p>Statut juridique : [Statut juridique de l'entreprise]</p>
+        <p>Adresse : [Adresse de l'entreprise]</p>
+        <p>Numéro de téléphone : [Numéro de téléphone de l'entreprise]</p>
+        <p>Email : [Adresse email de l'entreprise]</p>
+    </section>
+
+    <section>
+        <h2>Responsable de la publication</h2>
+        <p>Nom du responsable : [Nom du responsable]</p>
+        <p>Adresse : [Adresse du responsable]</p>
+        <p>Email : [Adresse email du responsable]</p>
+    </section>
+
+    <section>
+        <h2>Hébergement</h2>
+        <p>Nom de l'hébergeur : [Nom de l'hébergeur]</p>
+        <p>Adresse : [Adresse de l'hébergeur]</p>
+        <p>Numéro de téléphone : [Numéro de téléphone de l'hébergeur]</p>
+        <p>Email : [Adresse email de l'hébergeur]</p>
+    </section>
+
+    <section>
+        <h2>Propriété intellectuelle</h2>
+        <p>Tous les contenus présents sur ce site sont protégés par le droit d'auteur. Toute reproduction totale ou partielle sans autorisation préalable est interdite.</p>
+    </section>
+
+    <section>
+        <h2>Politique de confidentialité</h2>
+        <p>[Expliquez ici votre politique de confidentialité concernant les données collectées sur le site]</p>
+    </section>
+
+</main>
+
+
+<?php
+require_once("inc/footer.inc.php");
