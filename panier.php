5dedd5c788a056dd45c79a6a740598e4425f9bab
ikrame <ikr.projet@gmail.com>
2023-07-24 12:16:52 +0200

    update panier.php

--- panier.php
+++ panier.php
@@ -15,95 +15,108 @@ require_once("inc/category.inc.php");
 
 <main>
 
-<!DOCTYPE html>
-<html>
-<head>
+    <!DOCTYPE html>
+    <html>
 
-    <style>
-        body {
-            font-family: Arial, sans-serif;
-            margin: 0;
-            padding: 20px;
-            background-color: #f9f9f9;
-        }
-        
-        h1 {
-            text-align: center;
-        }
-        
-        table {
-            width: 100%;
-            border-collapse: collapse;
-            margin-bottom: 20px;
-        }
-        
-        th, td {
-            padding: 10px;
-            text-align: left;
-        }
-        
-        thead {
-            background-color: #333;
-            color: #fff;
-        }
-        
-        tbody tr:nth-child(even) {
-            background-color: #f2f2f2;
-        }
-        
-        tfoot {
-            font-weight: bold;
-        }
-        
-        button {
-            padding: 10px 20px;
-            background-color: #333;
-            color: #fff;
-            border: none;
-            cursor: pointer;
-        }
-        
-        button:hover {
-            background-color: #555;
-        }
-    </style>
-</head>
-<body>
-    
-    <table>
-        <thead>
-            <tr>
-                <th>Produit</th>
-                <th>Prix</th>
-                <th>Quantité</th>
-                <th>Total</th>
+    <head>
+        <style>
+            .panier {
+                border-color: black;
+                font-family: Arial, Helvetica, sans-serif;
+                display: grid;
+
+            }
+
+            .h2 {
+                text-align: left;
+                font-family: Arial, Helvetica, sans-serif;
+                margin-top: 100px;
+            }
+
+            .panier,h2 {
+                border-width: 50px;
+                border-color: black;
+            }
+
+            .ul {
+                list-style-type: none;
+            }
+
+        </style>
+    </head>
+
+    <body>
+        <main>
+            <div id="panier-container">
+                <div class="mon panier"></div>
+                <div class="title-panier">
+                </div>
+                <h2>MON PANIER</h2>
+                <ul>
+                    <li>Article</li>
+                    <li>Prix</li>
+                    <li>Quantité</li>
+                </ul>
+                <div class="btn-panier">
+                <tr>
+                    <td><img src="img/category/lunette-accessoires.jpg"></td>
+                    <span>260,99</span>
+                    <td>1</td>
+                    <td>
+                        <button>Modifier</button>
+                        <button>Supprimer</button>
+                    </td>
+                </div>
             </tr>
-        </thead>
-        <tbody>
-            <tr>
-                <td>Produit 1</td>
-                <td>29.99€</td>
-                <td>2</td>
-                <td>59.98€</td>
-            </tr>
-            <tr>
-                <td>Produit 2</td>
-                <td>19.99€</td>
-                <td>1</td>
-                <td>19.99€</td>
-            </tr>
-        </tbody>
-        <tfoot>
-            <tr>
-                <td colspan="3">Total:</td>
-                <td>79.97€</td>
-            </tr>
-        </tfoot>
-    </table>
-    
-    <button type="button">Passer à la caisse</button>
-</body>
-</html>
+                <tr>
+                    <td><img src="img/category/sneakers.jpg"></td>
+                    <td>269,99</td>
+                    <td>2</td>
+                    <td>
+                        <button>Modifier</button>
+                        <button>Supprimer</button>
+                    </td>
+                </tr>
+
+
+            </div>
+
+            <div id="resume">
+                <h2>Résumé de la commande</h2>
+                <table>
+                    <tbody>
+                        <tr>
+                            <td>Sous-total:</td>
+                            <td>500</td>
+                        </tr>
+                        <tr>
+                            <td>Frais de livraison:</td>
+                            <td>5€</td>
+                        </tr>
+                        <tr>
+                            <td><strong>Total:</strong></td>
+                            <td><strong>505€</strong></td>
+                        </tr>
+                    </tbody>
+                </table>
+                <input type="submit" value="Commander">
+            </div>
+
+            <div id="moyens-paiement-livraison">
+                <h2>Moyens de paiement et livraison</h2>
+                <p>Options de paiement et de livraison</p>
+            </div>
+
+            </div>
+
+            </div>
+            </div>
+
+            </section>
+        </main>
+    </body>
+
+    </html>
 
 
 
