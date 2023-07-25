e29db137f09db354a1e671e2d882b82bfaf4830c
Lenny <astienlenny@gmail.com>
2023-07-25 10:50:00 +0200

    Système de connexion

--- compte.php
+++ compte.php
@@ -10,6 +10,37 @@ require_once("inc/head.inc.php");
 require_once("inc/nav.inc.php");
 require_once("inc/category.inc.php");
 
+echo "<pre>";
+print_r($_POST);
+echo "</pre>";
+
+$message = "";
+$email = "";
+$password = "";
+
+
+$messageEmail = "";
+
+$msgError = array( "email" => "");
+
+if (isset( $_POST["email"])) {
+
+
+    
+    $email = trim($_POST["email"]);
+
+    $erreur = false;
+
+    if (empty($email)) {
+
+        $erreur = true;
+        $message .= '<div class="alert>
+        Attention tous les champs sont obligatoires !
+      </div>';
+    }
+
+}
+
 ?>
 
 
@@ -17,31 +48,34 @@ require_once("inc/category.inc.php");
     <div class="form1">
         <img src="assets/img/image-compte/coucher-de-soleil.jpg" alt="background">
         <!--FORMULAIRE-->
- <form method="$_POST" class="connexion">
+ <form action="traitement_connexion.php" method="POST" class="connexion">
     <h1>Mon compte</h1>
 
     <div class="champ">
 
-        <input type="email" placeholder="Email"> <br>
+        <?php if(isset($_GET['error']) && $_GET['error'] == 2) { ?>
+        <span style="color:red">ERREUR authentification</span><br>
+        <?php } ?>
+
+        <input type="email" placeholder="Email" name="email" required> <br>
         <label for="email"></label>
 
 
-        <input type="password" placeholder="Mot de passe"> <br>
+        <input type="password" placeholder="Mot de passe" name="mdp"> <br>
         <label for="password"></label>
         
         <a href="#" class="pw">Mot de passe oublié</a><br>
 
-        <input type="button" value="Connexion" style="
-         margin-top: 20px;"> 
+        <input type="submit" value="Connexion" style="
+         margin-top: 20px;">
     </div>
-
+</form>
    
         <div class="champ1">
            <h3>Pas de compte ?</h3>
         
-           <a href="inscription.php" class="compte"><input type="button" value="Créer un compte"></a>
+           <a href="inscription.php" class="compte"><input type="submit" value="Créer un compte"></a>
         </div>
-    </form>
  </form>
        
     </div>
@@ -51,3 +85,4 @@ require_once("inc/category.inc.php");
 
 <?php
 require_once("inc/footer.inc.php");
+?>
---
+++ traitement_connexion.php
@@ -0,0 +1,56 @@
+<?php
+
+echo "<pre>";
+print_r($_POST);
+echo "</pre>";
+
+//SELECT * FROM clients WHERE email=$_POST['email'] and password=$_POST['password']
+
+if(isset($_POST['email']) && isset($_POST['mdp']))
+{
+    //On se protége des injections XSS
+    $email = htmlentities($_POST['email']);
+    $mdp = htmlentities($_POST['mdp']);
+
+    //On se connecte pour la requete SQL
+    $host = "mysql:host=localhost;dbname=daikles"; // hôte + bdd
+    $login = "root"; // login
+    $password = "root"; // mot de passe
+    $options = array(
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING, // gestion des erreurs
+        PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8' // on force l'utf8 sur les données provenants de la bdd
+    );
+    // Création de l'objet PDO :
+    $pdo = new PDO($host, $login, $password, $options);
+    
+    //Faire la requête
+    $reponse = $pdo->prepare("SELECT * FROM clients WHERE email = :email and mdp=:mdp");
+    // Je donne ici la valeur attendue :
+    $reponse->bindParam(':email', $email, PDO::PARAM_STR); // le filtre PARAM_STR permet de transformer la saisie en 100% string
+    $reponse->bindParam(':mdp', $mdp, PDO::PARAM_STR); // il faudrait une ligne de bindParam par marqueur
+    // $reponse->bindParam(':pseudo', $pseudo, PDO::PARAM_STR);
+
+    // Maintenant que l'information attendue est fournie à la requête, on peut exécuter
+    $reponse->execute();
+
+    $donnees = $reponse->fetch(PDO::FETCH_ASSOC);
+
+    // echo "<pre>";
+    // print_r($donnees);
+    // echo "</pre>";
+
+    if(!empty($donnees) && !is_null($donnees))
+    {
+        die("BRAVO vous êtes connecté");
+        //ICI TU Créé la SESSION - Et tu redirige vers la page une fois connecté
+        //$_SESSION['user'] = $donnees;
+    }
+    else{
+        //die("OOps erreur d'authentification");
+        header("location:compte.php?error=2");
+    }
+
+}
+else{
+    header("location:compte.php?error=1");
+}
